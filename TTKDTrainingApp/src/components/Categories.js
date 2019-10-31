b54858aeeade98edfe7b345320577e4d50a36b0e
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-10-31 13:10:02 -0400

    [LK] - Add interaction to Categories; tapping a category goes to its subCategories

--- TTKDTrainingApp/src/components/Categories.js
+++ TTKDTrainingApp/src/components/Categories.js
@@ -1,26 +1,30 @@
 import React from 'react';
 
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CategoryItem = ({categoryName}) => {
   return (
     <View style={styles.categoryItem}>
       <Text style={styles.categoryText}>{categoryName}</Text>
-      <Icon name="arrow-right" />
+      <Icon name="angle-right" size={20} />
     </View>
   );
 };
 
 const Categories = props => {
-  const {categories} = props;
+  const {categories, setCategoryId} = props;
 
   return (
     <FlatList
       data={categories}
       keyExtractor={category => category.id}
       renderItem={({item}) => {
-        return <CategoryItem categoryName={item.category_name} />;
+        return (
+          <TouchableOpacity onPress={() => setCategoryId(item.id)}>
+            <CategoryItem categoryName={item.category_name} />
+          </TouchableOpacity>
+        );
       }}
       ItemSeparatorComponent={() => <View style={styles.categorySeparator} />}
     />
