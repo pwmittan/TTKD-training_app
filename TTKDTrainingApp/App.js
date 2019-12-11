cb7fcddc2836526dee42b944c2933b97a003b18d
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-12-11 15:14:33 -0500

    Adds HomeScreen to react navigation

--- TTKDTrainingApp/App.js
+++ TTKDTrainingApp/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
+import 'react-native-gesture-handler';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import HomeScreen from './src/components/HomeScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <React.Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <HomeScreen />
-      </SafeAreaView>
-    </React.Fragment>
-  );
-};
+// const App: () => React$Node = () => {
+//   return (
+//     <React.Fragment>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <AppContainer />
+//       </SafeAreaView>
+//     </React.Fragment>
+//   );
+// };
 
-export default App;
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+});
+
+export default createAppContainer(AppNavigator);
