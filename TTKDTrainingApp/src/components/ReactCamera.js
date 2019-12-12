fa3488e941920652df7a156406517750ecdfb724
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-12-11 22:09:06 -0500

    Adds ReactCamera component

---
+++ TTKDTrainingApp/src/components/ReactCamera.js
@@ -0,0 +1,120 @@
+import React, {PureComponent} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import {withNavigationFocus} from 'react-navigation';
+
+class ReactCamera extends PureComponent {
+  state = {
+    recording: false,
+    processing: false,
+  };
+  render() {
+    const {recording, processing} = this.state;
+
+    let button = (
+      <TouchableOpacity
+        onPress={this.startRecording.bind(this)}
+        style={styles.capture}>
+        <Text style={{fontSize: 14}}> RECORD </Text>
+      </TouchableOpacity>
+    );
+
+    if (recording) {
+      button = (
+        <TouchableOpacity
+          onPress={this.stopRecording.bind(this)}
+          style={styles.capture}>
+          <Text style={{fontSize: 14}}> STOP </Text>
+        </TouchableOpacity>
+      );
+    }
+
+    if (processing) {
+      button = (
+        <View style={styles.capture}>
+          <ActivityIndicator animating size={18} />
+        </View>
+      );
+    }
+    return (
+      this.props.isFocused && (
+        <View style={styles.container}>
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: 'Permission to use audio recording',
+              message: 'We need your permission to use your audio',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+          />
+          <View
+            style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+            {button}
+          </View>
+        </View>
+      )
+    );
+  }
+
+  async startRecording() {
+    this.setState({recording: true});
+    // default to mp4 for android as codec is not set
+    const {uri, codec = 'mp4'} = await this.camera.recordAsync();
+    this.setState({recording: false, processing: true});
+    const type = `video/${codec}`;
+
+    const data = new FormData();
+    data.append('video', {
+      name: 'mobile-video-upload',
+      type,
+      uri,
+    });
+
+    this.setState({processing: false});
+  }
+  stopRecording() {
+    this.camera.stopRecording();
+  }
+}
+
+export default withNavigationFocus(ReactCamera);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
