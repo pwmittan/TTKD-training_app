9f05daba29942dbf042f44e449ac4b19a5d1d36d
lucas.kretvix <lucas.kretvix@datadoghq.com>
2019-12-15 11:47:47 -0500

    Adds videos to mock_data, converts mock files to js for require(), reflects change in selectors and adds content methods

---
+++ TTKDTrainingApp/src/mock_data/categories.js
@@ -0,0 +1,47 @@
+export const categories = [
+  {
+    id: 1,
+    parent_id: null,
+    category_name: 'Basic Stuff',
+  },
+  {
+    id: 2,
+    parent_id: null,
+    category_name: 'Stances',
+  },
+  {
+    id: 3,
+    parent_id: null,
+    category_name: 'Blocks',
+  },
+  {
+    id: 4,
+    parent_id: null,
+    category_name: 'One-Steps',
+  },
+  {
+    id: 5,
+    parent_id: null,
+    category_name: 'Forms',
+  },
+  {
+    id: 6,
+    parent_id: 1,
+    category_name: 'Set up Information',
+  },
+  {
+    id: 7,
+    parent_id: 2,
+    category_name: 'Basic Stance Information',
+  },
+  {
+    id: 8,
+    parent_id: 2,
+    category_name: 'Advanced Stance Information (No Show)',
+  },
+  {
+    id: 9,
+    parent_id: null,
+    category_name: 'Empty Category (No Show)',
+  },
+];
--- TTKDTrainingApp/src/redux/selectors.js
+++ TTKDTrainingApp/src/redux/selectors.js
@@ -4,9 +4,13 @@ export const getHomeScreenState = store => store.homeScreen;
 export const getVideos = store => getReactCameraState(store).videos;
 
 const getAllCategories = store => getHomeScreenState(store).categories;
+const getAllContent = store => getHomeScreenState(store).content;
 
 export const getCategory = (store, categoryId) =>
   getAllCategories(store).find(category => category.id === categoryId);
 
 export const getSubCategories = (store, parentId) =>
   getAllCategories(store).filter(category => category.parent_id === parentId);
+
+export const getCategoryContent = (store, categoryId) =>
+  getAllContent(store).filter(content => content.category_id === categoryId);
