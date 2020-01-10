ee2e39c778ff47bc3f64ffb89f69a70a6b2b3af6
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-01-09 19:15:01 -0500

    Back button now cancels video recording and navigates to Content page without dispatching video; Video ending dispatches video and navigates to VideoWithControls page

--- TTKDTrainingApp/src/components/ReactCamera.js
+++ TTKDTrainingApp/src/components/ReactCamera.js
@@ -1,43 +1,66 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef, useEffect, useCallback} from 'react';
 import {useDispatch} from 'react-redux';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import {withNavigationFocus} from 'react-navigation';
+import {HeaderBackButton} from 'react-navigation-stack';
 
 import {addRecordedVideo} from './../redux/actions';
 
 const ReactCamera = props => {
+  const dispatch = useDispatch();
+
   const {isFocused, navigation} = props;
   const contentId = navigation.getParam('contentId');
   const maxLength = navigation.getParam('maxLength');
+  const shouldCancelVideo = navigation.getParam('shouldCancelVideo');
 
   const [recording, setRecording] = useState(false);
+  const [recordedVideo, setRecordedVideo] = useState(null);
 
+  // Handles starting recording
   useEffect(() => {
     if (recording === true) {
       startRecording();
     }
-  });
+  }, [recording, startRecording]);
 
-  const dispatch = useDispatch();
-  const dispatchAddRecordedVideo = recordedvideo =>
-    dispatch(addRecordedVideo({...recordedvideo, contentId}));
+  // Handles stopping video if back button is pressed
+  useEffect(() => {
+    shouldCancelVideo && cameraRef.current && cameraRef.current.stopRecording();
+  }, [shouldCancelVideo]);
 
-  const startRecording = async () => {
+  // Handles dispatching video and navigation once recording is stopped
+  useEffect(() => {
+    if (recordedVideo) {
+      if (!shouldCancelVideo) {
+        dispatch(addRecordedVideo({...recordedVideo, contentId}));
+        navigation.navigate('VideoWithControls', {
+          recordedVideo: recordedVideo,
+          contentId: contentId,
+        });
+      } else {
+        navigation.goBack();
+      }
+    }
+  }, [shouldCancelVideo, recordedVideo, contentId, dispatch, navigation]);
+
+  const startRecording = useCallback(async () => {
     // default to mp4 for android as codec is not set
-    const recordedvideo = await cameraRef.current.recordAsync({
-      mute: true,
-      maxDuration: maxLength,
-    });
+    try {
+      if (cameraRef.current) {
+        const video = await cameraRef.current.recordAsync({
+          mute: true,
+          maxDuration: maxLength,
+        });
 
-    dispatchAddRecordedVideo(recordedvideo);
-    setRecording(false);
-  };
-
-  // Simply commenting this code out in case we need to explicitly call stopRecording at any point
-  // const stopRecording = () => {
-  //   cameraRef.current.stopRecording();
-  // };
+        setRecording(false);
+        setRecordedVideo(video);
+      }
+    } catch (e) {
+      console.error('Recording Failed', e);
+    }
+  }, [maxLength]);
 
   const button = (
     <TouchableOpacity onPress={() => setRecording(true)} style={styles.capture}>
@@ -75,8 +98,16 @@ const ReactCamera = props => {
   );
 };
 
-ReactCamera.navigationOptions = {
-  title: 'Recording',
+ReactCamera.navigationOptions = ({navigation}) => {
+  return {
+    title: 'Recording',
+    headerLeft: () => (
+      <HeaderBackButton
+        tintColor="white"
+        onPress={() => navigation.setParams({shouldCancelVideo: true})}
+      />
+    ),
+  };
 };
 
 export default withNavigationFocus(ReactCamera);
