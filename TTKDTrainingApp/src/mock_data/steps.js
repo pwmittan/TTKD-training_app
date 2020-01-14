627ee1d7873314a278799730a89df0a9913c0cee
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-01-14 15:42:27 -0500

    Adds mock steps for Basic Stuff

---
+++ TTKDTrainingApp/src/mock_data/steps.js
@@ -0,0 +1,30 @@
+export const steps = [
+  {
+    id: 1,
+    content_id: 2,
+    start_time: 0,
+    end_time: 2,
+    text: 'Step one goes here',
+  },
+  {
+    id: 2,
+    content_id: 2,
+    start_time: 2,
+    end_time: 3,
+    text: 'Step two goes here',
+  },
+  {
+    id: 3,
+    content_id: 2,
+    start_time: 3,
+    end_time: 5,
+    text: 'Step three goes here',
+  },
+  {
+    id: 4,
+    content_id: 2,
+    start_time: 5,
+    end_time: 6,
+    text: 'Step four goes here',
+  },
+];
