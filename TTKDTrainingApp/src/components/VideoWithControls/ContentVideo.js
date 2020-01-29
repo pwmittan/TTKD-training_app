e8815843e26b39af325c3c40633490a66bd624b3
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-01-29 15:39:51 -0500

    Extracts content video to ContentVideo component

---
+++ TTKDTrainingApp/src/components/VideoWithControls/ContentVideo.js
@@ -0,0 +1,59 @@
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import Video from 'react-native-video';
+
+import {
+  getContentOwnVideoUri,
+  getContentOwnCachedVideoPath,
+} from '../../redux/selectors';
+import {genCachedUri} from '../../redux/actions';
+
+import {BASE_URI} from './constants';
+
+const ContentVideo = React.forwardRef((props, ref) => {
+  const dispatch = useDispatch();
+  const {
+    contentId,
+    paused,
+    rate,
+    handleLoad,
+    handleProgress,
+    handleEnd,
+    videoHeight,
+    videoWidth,
+  } = props;
+
+  const contentVideoUri = useSelector(state =>
+    getContentOwnVideoUri(state, contentId),
+  );
+  const cachedVideoPath = useSelector(state =>
+    getContentOwnCachedVideoPath(state, contentId),
+  );
+
+  useEffect(() => {
+    !cachedVideoPath &&
+      dispatch(genCachedUri(contentId, BASE_URI, contentVideoUri));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    cachedVideoPath && (
+      <Video
+        source={{
+          uri: cachedVideoPath,
+        }}
+        paused={paused}
+        rate={rate}
+        resizeMode="contain"
+        onLoad={handleLoad}
+        onError={e => console.info('Error on Video', e)}
+        onProgress={handleProgress}
+        onEnd={handleEnd}
+        ref={ref}
+        style={{height: videoHeight, width: videoWidth}}
+      />
+    )
+  );
+});
+
+export default ContentVideo;
