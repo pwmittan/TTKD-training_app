23cc6820fd16776c2a718ce30d50bc69f642914f
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-01-29 15:40:53 -0500

    Pulls in extracted components and implements them

--- TTKDTrainingApp/src/components/VideoWithControls/VideoWithControls.js
+++ TTKDTrainingApp/src/components/VideoWithControls/VideoWithControls.js
@@ -1,14 +1,10 @@
 import React, {useState, useRef, useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
 
 import {
   StyleSheet,
   View,
   Dimensions,
   TouchableWithoutFeedback,
-  Text,
-  FlatList,
-  TouchableOpacity,
   ActivityIndicator,
 } from 'react-native';
 import {withNavigationFocus} from 'react-navigation';
@@ -17,37 +13,18 @@ import {HeaderBackButton} from 'react-navigation-stack';
 import Video from 'react-native-video';
 
 import Controls from './Controls';
-import {
-  getContentOwnStepsSorted,
-  getContentOwnVideoUri,
-  getContentOwnCachedVideoPath,
-} from '../../redux/selectors';
-import {genCachedUri} from '../../redux/actions';
-import {PROGRESS_BAR_WIDTH} from './constants';
+import ContentVideo from './ContentVideo';
 
-const DEFAULT_SPEED = 1.0;
-const BASE_URI = 'https://ttkd-test-s3.s3.amazonaws.com/ttkd';
+import {DEFAULT_SPEED} from './constants';
+import Steps from './Steps';
 
 const VideoWithControls = props => {
-  const dispatch = useDispatch();
-
   /////////// Props/Navigation access////////////////
   const contentId =
     (props.navigation && props.navigation.getParam('contentId')) ||
     props.contentId;
   const recordedVideo =
     props.navigation && props.navigation.getParam('recordedVideo');
-
-  ////////////////// Selector Access /////////////////////
-  const steps = useSelector(state =>
-    getContentOwnStepsSorted(state, contentId),
-  );
-  const contentVideoUri = useSelector(state =>
-    getContentOwnVideoUri(state, contentId),
-  );
-  const cachedVideoPath = useSelector(state =>
-    getContentOwnCachedVideoPath(state, contentId),
-  );
 
   //////////////// State ////////////////////////
   const [paused, setPaused] = useState(true);
@@ -56,22 +33,7 @@ const VideoWithControls = props => {
   const [rate, setRate] = useState(DEFAULT_SPEED);
   const [loading, setLoading] = useState(true);
 
-  //Remove and do it a better way
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
-
   ///////////////////// Effects /////////////////////
-  useEffect(() => {
-    Array.isArray(steps) &&
-      steps.length &&
-      flatListRef.current &&
-      flatListRef.current.scrollToIndex({index: currentStepIndex});
-  }, [steps, currentStepIndex]);
-
-  useEffect(() => {
-    !cachedVideoPath &&
-      dispatch(genCachedUri(contentId, BASE_URI, contentVideoUri));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   useEffect(() => {
     if (!props.isFocused) {
@@ -105,9 +67,6 @@ const VideoWithControls = props => {
     setProgress(1);
   };
 
-  const handleStepPress = stepTime =>
-    Object.values(videoRefs).map(ref => ref.current.seek(stepTime));
-
   const {width} = Dimensions.get('window');
   const videoHeight = width * 0.5265;
   const fullHeight = recordedVideo ? videoHeight * 2 : videoHeight;
@@ -117,46 +76,8 @@ const VideoWithControls = props => {
     contentVideoRef: useRef(),
     ...(recordedVideo ? {recordedVideoRef: recordedVideoRef} : {}),
   };
-  const flatListRef = useRef();
 
   ///////////////////////////// Render Code //////////////////////////////
-
-  const renderSteps = (
-    <View style={styles.steps}>
-      <FlatList
-        onScrollToIndexFailed={error => {
-          console.info('onScrollToIndex failed', error);
-        }}
-        ref={flatListRef}
-        data={steps}
-        keyExtractor={step => `${step.id}`}
-        renderItem={({item, index}) => {
-          const progressSeconds = Math.round(progress * duration);
-          const isCurrentStep =
-            (!item.start_time || item.start_time <= progressSeconds) &&
-            (!item.end_time || item.end_time > progressSeconds);
-          isCurrentStep &&
-            index !== currentStepIndex &&
-            setCurrentStepIndex(index);
-          return (
-            <TouchableOpacity
-              onPress={() => handleStepPress(item.start_time || 0)}>
-              <View
-                style={[
-                  styles.step,
-                  isCurrentStep ? styles.currentStep : null,
-                ]}>
-                <Text style={styles.stepText}>
-                  {`${index + 1}: ${item.description}`}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
-    </View>
-  );
-
   return (
     <View style={styles.container}>
       {/* Videos */}
@@ -173,34 +94,16 @@ const VideoWithControls = props => {
                 style={{height: videoHeight, ...styles.video}}
               />
             )}
-            {cachedVideoPath && (
-              <Video
-                source={{
-                  uri: cachedVideoPath,
-                }}
-                paused={paused}
-                rate={rate}
-                // Because we are downloading and caching videos, there should realistically
-                // never be a time where the video needs to buffer
-                // onBuffer={() => {
-                //   console.info('Loading!');
-                //   setLoading(true);
-                // }}
-                // bufferConfig={{
-                //   minBufferMs: 15000,
-                //   maxBufferMs: 50000,
-                //   bufferForPlaybackMs: 2500,
-                //   bufferForPlaybackAfterRebufferMs: 5000,
-                // }}
-                resizeMode="contain"
-                onLoad={handleLoad}
-                onError={e => console.info('Error on Video', e)}
-                onProgress={handleProgress}
-                onEnd={handleEnd}
-                ref={videoRefs.contentVideoRef}
-                style={{height: videoHeight, ...styles.video}}
-              />
-            )}
+            <ContentVideo
+              contentId={contentId}
+              paused={paused}
+              rate={rate}
+              handleLoad={handleLoad}
+              handleProgress={handleProgress}
+              handleEnd={handleEnd}
+              videoHeight={videoHeight}
+              ref={videoRefs.contentVideoRef}
+            />
           </View>
         </TouchableWithoutFeedback>
       </View>
@@ -213,7 +116,12 @@ const VideoWithControls = props => {
         rate={rate}
         setRate={setRate}
       />
-      {renderSteps}
+      <Steps
+        videoRefs={videoRefs}
+        contentId={contentId}
+        progress={progress}
+        duration={duration}
+      />
       {/* Loading Indicator */}
       <ActivityIndicator
         style={{top: fullHeight / 2 - 16, ...styles.activityIndicator}}
@@ -235,22 +143,6 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 
-  steps: {
-    height: 50,
-    width: '100%',
-    flex: 1,
-    justifyContent: 'flex-start',
-  },
-  step: {
-    paddingHorizontal: 8,
-    paddingVertical: 2,
-  },
-  currentStep: {
-    backgroundColor: '#C4BBBB',
-  },
-  stepText: {
-    fontSize: 16,
-  },
   activityIndicator: {
     position: 'absolute',
   },
