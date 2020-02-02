3ee90f87c791d8d609b3046193e539188ab5fc53
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-02-02 13:29:48 -0500

    Uses thumbnail_path for ContentCard thumbnail

--- TTKDTrainingApp/src/components/ContentCard.js
+++ TTKDTrainingApp/src/components/ContentCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {BASE_S3_URI} from '../api/API';
 
 const ContentCard = props => {
   const {navigation, content} = props;
+  const s3ImageUrl =
+    content.thumbnail_path &&
+    `${BASE_S3_URI}/${content.title}/${content.thumbnail_path}`.replace(
+      / /g,
+      '%20',
+    );
+
   return (
     <TouchableOpacity
       onPress={() => navigation.navigate('Content', {content: content})}>
       <View style={styles.content}>
         <Text style={styles.contentText}>{content.title}</Text>
         <Image
-          source={Image.resolveAssetSource(content.video)}
+          source={{
+            uri: s3ImageUrl,
+          }}
           style={styles.contentImage}
         />
         <Icon name="angle-right" size={24} />
@@ -30,7 +40,6 @@ const styles = StyleSheet.create({
   contentImage: {
     height: 50,
     width: 50,
-    backgroundColor: 'red',
   },
 });
 
