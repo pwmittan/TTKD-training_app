663e152cc94918f89eda4a58963cf8292aa944e1
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-02-02 11:34:55 -0500

    Installs react-native-orientation-locker

--- TTKDTrainingApp/android/app/src/main/java/com/ttkdtrainingapp/MainActivity.java
+++ TTKDTrainingApp/android/app/src/main/java/com/ttkdtrainingapp/MainActivity.java
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,6 +17,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TTKDTrainingApp";
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
   }
 
   @Override
