993d7ca781b7e45b32cc12dbf18c479cd9a65cd2
lucas.kretvix <lucas.kretvix@datadoghq.com>
2020-02-15 14:02:32 -0500

    Adds mocked studio validation method in API; Adds s3 url generator method to API

--- TTKDTrainingApp/src/api/API.js
+++ TTKDTrainingApp/src/api/API.js
@@ -1,7 +1,18 @@
-export const BASE_S3_URI = 'https://ttkd-test-s3.s3.amazonaws.com/ttkd';
+export const BASE_S3_URI = 'https://ttkd-test-s3.s3.amazonaws.com';
 
-export const fetchAppData = async () => {
+export const validateStudioCode = async studio => {
+  // currently mocked with a studio mapping, but will fetch from an API in the future
+  const studios = {
+    TTKD: 'ttkd',
+  };
+  return studios.hasOwnProperty(studio) && studios[studio];
+};
+
+export const fetchAppData = async studio => {
   return await fetch(
-    'https://sfjy3c2yji.execute-api.us-east-1.amazonaws.com/TestDBFetchall',
+    `https://sfjy3c2yji.execute-api.us-east-1.amazonaws.com/TestDBFetchall?studio=${studio}`,
   );
 };
+
+export const generateS3URL = (studio, title, path) =>
+  `${BASE_S3_URI}/${studio}/${title}/${path}`.replace(/ /g, '%20');
